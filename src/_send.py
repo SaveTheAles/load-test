dfa01dd480a0abd01301f90455eb68c8a20ca8eb
Ales Puchilo <ales.puchilo@gmail.com>
2021-08-20 22:30:34 +0200

    fix amper send balancer

--- src/_send.py
+++ src/_send.py
@@ -4,9 +4,10 @@ from src._minion_helper import get_balance
 from config import FRIENDS
 
 def get_send_data(friends:dict):
+    res = []
     for friend in friends.items():
-        friend: get_balance(friend[0])
-    address = min(friends, key=friends.get)
+        res.append((friend[0], int(get_balance(friend[0]))))
+    address = min(res, key=lambda x: x[1])[0]
     amount = random.randint(1, 3)
     name = FRIENDS[address]
     return address, amount, name
